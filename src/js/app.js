6731f14852f8238947d3b2b39ec198318802a404
Evgeniya Khrsanfova <efkhrisanfova@gmail.com>
2024-01-27 01:32:05 +0300

    correct

--- src/js/app.js
+++ src/js/app.js
@@ -2,15 +2,17 @@ export default function orderByProps(object, order) {
   const arrayStart = [];
   const arrayEnd = [];
   for (const prop in object) {
-    const item = {
-      key: prop,
-      value: object[prop],
-    };
-    const index = order.indexOf(prop);
-    if (index >= 0) {
-      arrayStart[index] = item;
-    } else {
-      arrayEnd.push(item);
+    if (object.hasOwnProperty(prop)) {
+      const item = {
+        key: prop,
+        value: object[prop],
+      };
+      const index = order.indexOf(prop);
+      if (index >= 0) {
+        arrayStart[index] = item;
+      } else {
+        arrayEnd.push(item);
+      }
     }
   }
   arrayEnd.sort((a, b) => (a.key > b.key ? 1 : -1));
